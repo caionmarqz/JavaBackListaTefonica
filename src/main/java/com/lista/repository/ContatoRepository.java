15c5bbdbc1f0b79fc0543ccd3b96ecd32a7f44a6
Caio Marquezini <caio@localhost.localdomain>
2021-02-07 03:21:48 -0300

    first branch

---
+++ src/main/java/com/lista/repository/ContatoRepository.java
@@ -0,0 +1,21 @@
+package com.lista.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.lista.model.Contato;
+import com.teste.cadastro.model.Cliente;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Long>{
+	
+	@Query(value = "SELECT a FROM Contato a WHERE a.nome LIKE %:nome%", nativeQuery = false)
+	List<Contato> findByNome(String nome);
+	
+//	@Query("SELECT w FROM Cliente w WHERE w.nome LIKE %:search%")
+//	List<Cliente> findByName(String search);
+	
+}
